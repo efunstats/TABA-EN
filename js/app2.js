ac2de2ed2c5e7407495a01619582bfb59569107a
efunstats <45446150+efunstats@users.noreply.github.com>
2018-11-29 00:54:25 -0500

    Add files via upload

--- js/app2.js
+++ js/app2.js
@@ -173,7 +173,6 @@ var app = new Vue({
                     this.cards.push(temp);
                 }    
             })
-
             this.skills=[];
             fetch('https://spreadsheets.google.com/feeds/list/1HETOccLbHyBhnGbdQ44MwIuEBsv1m2Qk6URTxTCCyD0/2/public/values?alt=json')
             .then(res => res.json())
